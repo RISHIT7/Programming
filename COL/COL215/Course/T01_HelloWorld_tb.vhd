445220930fcb84ff9add5f1a1929a9884eb4c77d
Rishit Jakharia <rishitjakharia@gmail.com>
2023-07-29 09:53:57 +0530

    vhdlll

---
+++ COL/COL215/Course/T01_HelloWorld_tb.vhd
@@ -0,0 +1,17 @@
+-- Calling the entity somewhat like in java, for input and output
+ENTITY T01_HelloWorld_tb IS
+END ENTITY;
+
+-- Building the architecture, for most computation that is the algorithms
+ARCHITECTURE sim OF T01_HelloWorld_tb IS
+BEGIN
+
+PROCESS IS
+BEGIN
+
+REPORT "Hello World!";
+WAIT;
+
+END PROCESS;
+
+END ARCHITECTURE;
