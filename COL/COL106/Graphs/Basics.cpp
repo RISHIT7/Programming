f27566be6a677ac0645e63f9e04fc669fe71773c
Rishit Jakharia <rishitjakharia@gmail.com>
2023-07-29 14:12:22 +0530

    GB

---
+++ COL/COL106/Graphs/Basics.cpp
@@ -0,0 +1,27 @@
+/*
+Nodes
+
+Edges
+    Undirected - Two way edges <->
+    Directed - One way edges ->
+
+Representation
+    Adjacency Matrix
+    Adjacency list
+
+DFS
+    PreOrder
+        First time when call comes to the node
+    InOrder
+        When the call wors inside the subtree of that node
+    PostOrder
+        After working on the subtree, call comes, back on the node. 
+        After this, call won't come on the same node
+    
+    Done using Stacks
+
+BFS
+    Done using Queue
+    Weighted Nodes - Values are associated with nodes
+    weighted Edges - Values are associated with Edges
+*/
