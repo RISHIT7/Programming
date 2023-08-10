c57f9b85fd8fcededff4c0e14b6fd4eaadfcf0ef
Rishit Jakharia <rishitjakharia@gmail.com>
2023-08-10 16:49:20 +0530

    saaving mehnat

--- COL/COL215/Labs/SWLab1/Part1/understanding.py
+++ COL/COL215/Labs/SWLab1/Part1/understanding.py
@@ -43,14 +43,37 @@ D 2 -F
 [[C, B], 4, D]
 [[C, D], 3, E]
 [[D], 2, F]
+
+A --> C
+C --> D <-- B
+C --> E <-- D
+D --> F
+
+
 """
 
-def checker(array):
-    input_array = array[0]
-    for el in input_array:
-        if el not in ["A", "B"]:
-            return False
-    return True
+# def checker(array):
+#     input_array = array[0]
+#     for el in input_array:
+#         if el not in ["A", "B"]:
+#             return False
+#     return True
 
-print(checker([["A"], 2, "C"]))
+# print(checker([["A"], 2, "C"]))
 
+# def return_inputs(array):
+#     return array[0]
+
+# def find_output(el):
+#     pass
+
+# def find_input(master_array, array):
+#     inputs = return_inputs(array)
+#     if (checker(array)):
+#         return array[1]
+#     else:
+#         for el in inputs:
+#             if (el != "A" and el != "B"):
+#                 list = find_output(el)
+#                 array[0].remove(el)
+#                 array[1] += list[1]
