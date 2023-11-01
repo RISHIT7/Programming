aafc81648f36154847c421a08c7822704a4a1182
Rishit Jakharia <rishitjakharia@gmail.com>
2023-11-02 05:07:56 +0530

    Commit #0002
    Read ASCII Files

---
+++ COL/COL106/LanguageModel/staple/docs/current.md
@@ -0,0 +1,9 @@
+# Staple VM Spec
+
+'#' means number contant
+
+## Instuctions
+
+* PUSH - Push an 8 bit number onto the stack
+* ADD - Pops last two numebr off of stack, adds, pushes, result
+* HLT - Pops stack and return result as exit code
---
+++ COL/COL106/LanguageModel/staple/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/util.h"
+
+// staple compile file.stvm
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        printf("Too few arguments\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "compile") == 0)
+    {
+        char *source = read_ascii_file(argv[2]);
+        free(source);
+    }
+
+    return 0;
+}
