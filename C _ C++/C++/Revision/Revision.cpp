c270b3b5296abcf6bfdec583929dc3879e507d08
Rishit Jakharia <rishitjakharia@gmail.com>
2023-11-02 04:34:05 +0530

    Making a new language

--- C _ C++/C++/Revision/Revision.cpp
+++ C _ C++/C++/Revision/Revision.cpp
@@ -7,6 +7,13 @@ int main()
     cout << "Welcome to a program with a bug !\n";
     cin >> d;
     cout << "You gave me: " << d << "\n";
+    int count = 0;
+    while (d < 100)
+    {
+        d++;
+        count++;
+    }
+    cout << "Value of count is " << count << "\n";
 
     return 0;
 }
