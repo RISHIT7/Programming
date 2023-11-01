6b4a28f87f060fcbbe2c1fe00d367f8d3f11dd9d
Rishit Jakharia <rishitjakharia@gmail.com>
2023-11-02 03:45:47 +0530

    Commit

--- C _ C++/C++/Revision/Revision.cpp
+++ C _ C++/C++/Revision/Revision.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int d = 2;
+    cout << "Welcome to a program with a bug !\n";
+    cin >> d;
+    cout << "You gave me: " << d << "\n";
+
+    return 0;
+}
