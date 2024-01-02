e94d5aa67dfdabfe5d942826222c62b06f67b9de
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-02 13:21:07 +0530

    Booth's Algo

--- COL/COL216/Assign1_test_1.py
+++ COL/COL216/Assign1_test_1.py
@@ -8,7 +8,6 @@ def binary_fixed_point_multiply(a, b, num_frac_bits, num_int_bits):
             result += a
 
         # Right shift multiplier
-        print(bin(b))
         b >>= 1
 
     # Adjust the result for the fractional bits
@@ -16,21 +15,27 @@ def binary_fixed_point_multiply(a, b, num_frac_bits, num_int_bits):
 
     return result
 
-# Example with binary input
-binary_multiplier = 0b0011001101000100 #"00110011.01000100"
-binary_multiplicand = 0b0010001000010001 #"00100010.00010001"
 
-# Remove the dot and get the total number of bits
+def main():
+            
+    # Example with binary input
+    binary_multiplier = 0b0011001101000100 #"00110011.01000100"
+    binary_multiplicand = 0b0010001000010001 #"00100010.00010001"
 
-# Adjust the input for the fixed-point representation
-multiplier = binary_multiplier
-multiplicand = binary_multiplicand
+    # Remove the dot and get the total number of bits
 
-result = binary_fixed_point_multiply(multiplier, multiplicand, 8, 8)
+    # Adjust the input for the fixed-point representation
+    multiplier = binary_multiplier
+    multiplicand = binary_multiplicand
 
-# Convert result back to binary fixed-point representation
-result_binary = bin(result)[2:].zfill(32)
+    result = binary_fixed_point_multiply(multiplier, multiplicand, 8, 8)
 
-print("Binary Multiplier:", bin(binary_multiplier)[2:].zfill(16))
-print("Binary Multiplicand:", bin(binary_multiplicand)[2:].zfill(16))
-print(f"Result (Binary):", result_binary)
+    # Convert result back to binary fixed-point representation
+    result_binary = bin(result)[2:].zfill(32)
+
+    print(f"Binary Multiplier: {(bin(binary_multiplier)[2:].zfill(16))[:8]}.{(bin(binary_multiplier)[2:].zfill(16))[8:]}")
+    print(f"Binary Multiplicand: {(bin(binary_multiplicand)[2:].zfill(16))[:8]}.{(bin(binary_multiplicand)[2:].zfill(16))[8:]}")
+    print(f"Result (Binary) {result_binary[:16]}.{result_binary[16:]}")
+
+if __name__ == "__main__":
+    main()
