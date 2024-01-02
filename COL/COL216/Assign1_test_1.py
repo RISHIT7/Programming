4e0849e60b07916b1d11566c80440c833ed6cb68
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-01-02 10:43:48 +0530

    Update Assign1_test_1.py

--- COL/COL216/Assign1_test_1.py
+++ COL/COL216/Assign1_test_1.py
@@ -17,8 +17,8 @@ def binary_fixed_point_multiply(a, b, num_frac_bits, num_int_bits):
     return result
 
 # Example with binary input
-binary_multiplier = 0b00110100 #"0011.0100"
-binary_multiplicand = 0b00100001 #"0010.0001"
+binary_multiplier = 0b0011001101000100 #"00110011.01000100"
+binary_multiplicand = 0b0010001000010001 #"00100010.00010001"
 
 # Remove the dot and get the total number of bits
 
@@ -26,11 +26,11 @@ binary_multiplicand = 0b00100001 #"0010.0001"
 multiplier = binary_multiplier
 multiplicand = binary_multiplicand
 
-result = binary_fixed_point_multiply(multiplier, multiplicand, 4, 4)
+result = binary_fixed_point_multiply(multiplier, multiplicand, 8, 8)
 
 # Convert result back to binary fixed-point representation
-result_binary = bin(result)[2:].zfill(16)
+result_binary = bin(result)[2:].zfill(32)
 
-print("Binary Multiplier:", bin(binary_multiplier)[2:].zfill(8))
-print("Binary Multiplicand:", bin(binary_multiplicand)[2:].zfill(8))
+print("Binary Multiplier:", bin(binary_multiplier)[2:].zfill(16))
+print("Binary Multiplicand:", bin(binary_multiplicand)[2:].zfill(16))
 print(f"Result (Binary):", result_binary)
