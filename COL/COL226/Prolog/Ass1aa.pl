bb9139609dabc1197a643f877a72e3c41904e507
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-14 14:46:34 +0530

    testing prolog

--- COL/COL226/Prolog/Ass1aa.pl
+++ COL/COL226/Prolog/Ass1aa.pl
@@ -8,17 +8,22 @@ mem(X, [_|R]) :- mem(X, R).
 /* Reflexive_Transitive */
 mem((X, X), rc(R, S)) :- mem(X, S), !.
 mem((X, Y), rc(R, S)) :- mem((X, Y), R), !.
+/* R^0 */
 
 mem((X, Y), tc(R, S)) :- mem((X, Y), R), !.
 mem((X, Z), tc(R, S)) :- mem((X, Y), R), mem((Y, Z), tc(R, S)), !.
+/* UR^i */
 
-mem((X, Y), rtc(R, S)) :- mem((X, Y), rc(R, S)), !.
-mem((X, Y), rtc(R, S)) :- mem((X, Y), tc(R, S)), !.
+mem((X, X), rtc(R, S)) :- mem(X, S), !.
+mem((X, Y), rtc(R, S)) :- mem((X, Y), R), !.
+mem((X, Z), rtc(R, S)) :- mem((X, Y), R), mem((Y, Z), rtc(R, S)), !.
 
 /* Ref_Trans_Symm */
 mem((X, Y), sc(R, S)) :- mem((X, Y), R), !.
 mem((Y, X), sc(R, S)) :- mem((X, Y), R), !.
 
-mem((X, Y), rtsc(R, S)) :- mem((X, Y), rc(R, S)), !.
-mem((X, Y), rtsc(R, S)) :- mem((X, Y), tc(R, S)), !.
-mem((X, Y), rtsc(R, S)) :- mem((X, Y), sc(R, S)), !.
+mem((X, X), rtsc(R, S)) :- mem(X, S), !.
+mem((X, Y), rtsc(R, S)) :- mem((X, Y), R), !.
+mem((X, Y), rtsc(R, S)) :- mem((Y, X), R), !.
+mem((X, Z), rtsc(R, S)) :- mem((X, Y), R), mem((Y, Z), rtsc(R, S)), !.
+mem((X, Z), rtsc(R, S)) :- mem((Y, X), R), mem((Z, Y), rtsc(R, S)), !.
---
+++ COL/COL226/Prolog/Ass1ab.pl
@@ -0,0 +1,26 @@
+/*  del(X,L1,L2) -- delete element X from a list L1 to obtain L2 */ 
+del(X, [ ] , [ ]) :- !.
+del(X, [X|R], Z) :- del(X, R, Z), !.
+del(X, [Y|R], [Y|Z]) :- del(X, R, Z), !.
+
+/*  remdups(L, L1) remove duplicates from a list L to get L1 */
+remdups([ ], [ ]) :- !.
+remdups([X|R], [X|Z]) :- del(X, R, L), remdups(L, Z).
+
+/* Assuming no duplicates in S1, S2
+ here is an implementation of union of S1, S2 */
+unionI([ ], S2, S2) :- !.
+unionI(S1, [ ], S1) :- !.
+unionI([X|R], S2, [X|Z]) :- del(X, S2, S3),  unionI(R, S3, Z).
+
+/* append(L1, L2, L3) -- append lis  L1 to list L2 to get list  L3 */
+append( [ ], L, L).
+append( [X|R], L, [X|Z]) :- append(R, L, Z).
+
+/* mapcons(X,L1, L2) --  cons the element X to each list in L1 to get L2 */
+mapcons(X, [ ], [ ]) :- !.
+mapcons(X, [Y|R], [ [X|Y] | Z ]) :- mapcons(X, R, Z).
+
+/* powerI( S, P1): Here is an implementation of powerset of S */
+powerI([ ], [ [ ] ]) :- !.
+powerI([X|R], P) :- powerI(R, P1),  mapcons(X, P1, P2), append(P2, P1, P).
