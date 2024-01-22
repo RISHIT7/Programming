1a1ae217f4c93ff4e5ae6a88bfa7985eafdffeee
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-22 15:42:01 +0530

    csv done

--- COL/COL106/Revision/Revision.cpp
+++ COL/COL106/Revision/Revision.cpp
@@ -30,43 +30,12 @@ using namespace std;
 
 const int N = 1e5 + 2, MOD = 1e9 + 7;
 
-struct node
+int main()
 {
-    int data;
-    node* right;
-    node* left;
-
-    node(int val)
-    {
-        data = val;
-        right = NULL;
-        left = NULL;
-    }
-};
-
-node* insertBST(node* root, int val)
-{
-    if (root == NULL)
-    {
-        root = new node(val);
-        return root;
-    }
-    if (root->data < val){
-        return insertBST(root->left, val);
-    }
-    else {
-        return insertBST(root->right, val);
-    }    
-    return root;
-}
-
-int main() {
-    node* root = NULL;
-    root = insertBST(root, 5);
-    insertBST(root, 1);
-    insertBST(root, 3);
-    insertBST(root, 4);
-    insertBST(root, 2);
-    insertBST(root, 7);
+    float n;
+    cin >> n;
+    int m;
+    cin >> m;
+    cout << (int) n/m << endl;
     return 0;
 }
