e5ee935e2791955e2d7e7559b6fdf6dd541dd110
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-23 16:24:44 +0530

    made a bunch of flash messages

--- Python/Flask/Website_tut/website/auth.py
+++ Python/Flask/Website_tut/website/auth.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 # Used for UI, and have all the URLs
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+# by default method accepted are only GET, but now, we can accept both GET and POST requests
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    # we only want to store info in a POST request
+    if request.method == 'POST':
+        email = request.form.get('email') # names
+        password = request.form.get('password')
+        
+        
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -13,4 +20,24 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        # now we want to check if the info is valid
+        if len(email) < 4:
+            # message flashing
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First Name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords must match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 6 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category='success')
+        
     return render_template("sign_up.html")
