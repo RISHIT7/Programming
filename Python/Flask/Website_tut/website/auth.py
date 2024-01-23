5bff85dc77dfa56e1009d6991414dc36f32384e9
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-23 16:30:46 +0530

    fixed bugs

--- Python/Flask/Website_tut/website/auth.py
+++ Python/Flask/Website_tut/website/auth.py
@@ -18,7 +18,7 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
