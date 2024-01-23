890f7fa197d6a1602e3887183867d40e8ebb4ecc
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-23 17:22:37 +0530

    Done with the foreign key

--- Python/Flask/Website_tut/website/__init__.py
+++ Python/Flask/Website_tut/website/__init__.py
@@ -3,9 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'gsldf glsdfgsjfg'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
--- Python/Flask/Website_tut/website/models.py
+++ Python/Flask/Website_tut/website/models.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func # get's the current date and time
+# . is the current "website" package, and there in the __init__ file we have the "db" defined
 
-app = Flask(__name__)
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone = True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # always stores id of one of the users
+    # one to many relationship, here, one object has many children, that is onr user has many notes
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug = True)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(150), unique = True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
