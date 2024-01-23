6eeae459d66488120cc295e5e4f900b35bf3280a
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-23 18:17:40 +0530

    added current_user feature

--- Python/Flask/Website_tut/website/views.py
+++ Python/Flask/Website_tut/website/views.py
@@ -8,4 +8,4 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home(): # this function runs when we go to the above route in the decorator
-    return render_template("home.html")
+    return render_template("home.html", user = current_user)
