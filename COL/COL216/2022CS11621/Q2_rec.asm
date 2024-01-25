db39efd65523068dbe6a793fb31740f43066005c
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-25 20:32:14 +0530

    Q1, Q2[A][B] done

---
+++ COL/COL216/2022CS11621/Q2_rec.asm
@@ -0,0 +1,39 @@
+.global _start
+_start:
+	li a7, 5
+	ecall
+	
+	li t0, 0
+	# store the arguments of the function in a0
+
+sum:
+	# creating space in the stack for this variable
+	addi sp, sp, -8
+	
+	# if n == 0: return 0;	
+	li a1, 0
+	beqz a0, end_func
+	
+	# return n * sum(n-1)
+	sw a0, 0(sp)
+	addi a0, a0, -1
+	addi t0, t0, 1
+	j sum
+
+end_func:
+	# pop things off the stack
+	addi sp, sp, 8
+	lw a0, 0(sp)
+	
+	add a1, a1, a0
+	addi t0, t0, -1
+	bnez t0, end_func
+	
+end:
+	mv a0, a1
+	li a7, 1
+	ecall
+
+	li a7, 93
+	li a0, 42
+	ecall
