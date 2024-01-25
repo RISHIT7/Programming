4148199b9caf8906ca07e5faf080d38c6ac5ae06
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-25 19:08:05 +0530

    firsst commit of the day

--- COL/COP290/Flask_Implementation/website/__init__.py
+++ COL/COP290/Flask_Implementation/website/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -8,24 +7,25 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'gsldf glsdfgsjfg'
+    app.config['SECRET_KEY'] = "Starlord_Love_hurts_but_42_doesn't"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
     from .views import views
     from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
     
     # to check if database is created
-    from .models import User, Note
+    from .models import User
     
     with app.app_context():
         db.create_all()
         
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login' # where do we go if not logged in
+    login_manager.login_view = 'auth.register' # where do we go if not logged in
     login_manager.init_app(app) # telling the login manager, which app we are using
     
     # this is telling flask how we load a user
---
+++ COL/COP290/Flask_Implementation/website/views.py
@@ -0,0 +1,12 @@
+# this file handels the frontend that is the home page
+from flask import Blueprint, jsonify, render_template, request, flash
+from flask_login import login_required, current_user
+from . import db
+import json 
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods = ['GET', 'POST'])
+@login_required
+def home():
+    return render_template("home.html", user = current_user)
