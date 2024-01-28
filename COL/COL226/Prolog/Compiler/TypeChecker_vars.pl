1831adff85323a507d86099d2667ccfe248660ba
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-29 04:56:26 +0530

    Assignment Completed

--- COL/COL226/Prolog/Compiler/TypeChecker_vars.pl
+++ COL/COL226/Prolog/Compiler/TypeChecker_vars.pl
@@ -3,13 +3,14 @@ type(boolT).
 
 hastype(_, intT(N), intT) :- integer(N), !.
 hastype(_, boolT(B), boolT) :- member(B, [true, false]), !.
+hastype(G, varT(X), T) :- member((X, T), G), !.
 
-hastype(G, plus(E1, E2), intT) :- hastype(G, E1, intT), hastype(G, E2, intT).
-hastype(G, times(E1, E2), intT) :- hastype(G, E1, intT), hastype(G, E2, intT).
+hastype(G, plus(E1, E2), intT) :- hastype(G, E1, intT), hastype(G, E2, intT), !.
+hastype(G, times(E1, E2), intT) :- hastype(G, E1, intT), hastype(G, E2, intT), !.
 
-hastype(G, or(E1, E2), boolT) :- hastype(G, E1, boolT), hastype(G, E2, boolT).
-hastype(G, and(E1, E2), boolT) :- hastype(G, E1, boolT), hastype(G, E2, boolT).
-hastype(G, not(E1), boolT) :- hastype(G, E1, boolT).
+hastype(G, or(E1, E2), boolT) :- hastype(G, E1, boolT), hastype(G, E2, boolT), !.
+hastype(G, and(E1, E2), boolT) :- hastype(G, E1, boolT), hastype(G, E2, boolT), !.
+hastype(G, not(E1), boolT) :- hastype(G, E1, boolT), !.
 
-hastype(G, eq(E1, E2), boolT) :- hastype(G, E1, intT), hastype(G, E2, intT).
-hastype(G, gt(E1, E2), boolT) :- hastype(G, E1, intT), hastype(G, E2, intT).
+hastype(G, eq(E1, E2), boolT) :- hastype(G, E1, intT), hastype(G, E2, intT), !.
+hastype(G, gt(E1, E2), boolT) :- hastype(G, E1, intT), hastype(G, E2, intT), !.
