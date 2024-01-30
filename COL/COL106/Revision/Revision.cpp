1173a7c4a53e9cb263fe9d70628a9f104e3c5bc9
Rishit Jakharia <rishitjakharia@gmail.com>
2024-01-30 11:02:34 +0530

    fast commit

--- COL/COL106/Revision/Revision.cpp
+++ COL/COL106/Revision/Revision.cpp
@@ -30,12 +30,83 @@ using namespace std;
 
 const int N = 1e5 + 2, MOD = 1e9 + 7;
 
-int main()
-{
-    float n;
-    cin >> n;
-    int m;
-    cin >> m;
-    cout << (int) n/m << endl;
+struct node{
+    int data;
+    struct node* right ;
+    struct node* left ;
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }    
+};
+
+void swap(int* a, int* b ){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void calcPointers(node* root, node** first, node** mid, node** last, node** prev){
+    if(root==NULL){
+        return ;
+    }
+    calcPointers(root->left, first, mid, last, prev);
+
+    if(prev && root->data < (*prev)->data){
+        if(!*first){
+            *first = *prev;
+            *mid=root;
+        }
+    else{
+        *last = root;
+    }
+    }
+    *prev = root;
+
+    calcPointers(root->right, first, mid, last, prev);
+
+}
+
+void RestoreBST(node* root){
+    node* first=NULL;
+    node* mid=NULL;
+    node* last=NULL;
+    node* prev=NULL;
+    
+    calcPointers(root, &first, &mid, &last, &prev);
+
+    if(first && last){
+        swap(&(first->data), &(last->data));
+    }
+    else if(first && mid){
+        swap(&(first->data), &(mid->data));
+    }
+}
+
+void inorder(node* root){
+    if(root==NULL){
+        return ;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+int main(){
+    node* root = new node(6);
+    root->left = new node(9);
+    root->right = new node(3);
+    root->left->left = new node(1);
+    root->left->right = new node(4);
+    root->right->right = new node(13);
+
+    inorder(root);
+    cout<<endl;
+    RestoreBST(root);
+    
+    inorder(root);
+    cout<<endl;
+
     return 0;
 }
