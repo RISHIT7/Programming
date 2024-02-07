2b6d1d047ebc46d9630295eab4f191427d26819a
Rishit Jakharia <rishitjakharia@gmail.com>
2024-02-07 17:03:00 +0530

    Python file running

--- COL/COP290/Stratergies/main.py
+++ COL/COP290/Stratergies/main.py
@@ -2,12 +2,21 @@ import sys
 from datetime import date
 import pandas as pd
 from datetime import date
+from jugaad_data.nse import stock_df
 # pyarrow, h5py, tables -> (pytables), feather-format -> (feather), fastavro -> (avro), seaborn, matplotlib
 
 # ------------------------------------ generation of data frame --------------------------------------------
-def generate_dataframe(to_date, symbol):
-    from jugaad_data.nse import stock_df
-    df = pd.DataFrame(stock_df(symbol=symbol, from_date=date(to_date.year-int(sys.argv[2]),to_date.month,to_date.day), to_date=date(to_date.year,to_date.month,to_date.day), series="EQ"))
+def generate_dataframe(symbol, today, lastday):
+    # formatting the date
+    to_day = int(today[:2])
+    to_month = int(today[3:5])
+    to_year = int(today[6:10])
+    
+    la_day = int(lastday[:2])
+    la_month = int(lastday[3:5])
+    la_year = int(lastday[6:10])
+    
+    df = pd.DataFrame(stock_df(symbol=symbol, from_date=date(to_year, to_month, to_day), to_date=date(la_year, la_month, la_day), series="EQ"))
     df = df[[ "DATE", "OPEN", "CLOSE", "HIGH","LOW", "LTP", "VOLUME", "VALUE","NO OF TRADES"]]
     return df
 
@@ -18,10 +27,11 @@ def write_pickle(DATA, symbol):
 
 # -------------------------------------------------------- MAIN -----------------------------------------------------------------------
 def main():
-    today = date.today()
     argument = sys.argv[1]
+    today = sys.argv[2]
+    lastday = sys.argv[3]
 
-    DATA = generate_dataframe(today, argument)
+    DATA = generate_dataframe(argument, today, lastday)
     write_pickle(DATA, "Stocks/" + argument)
 
 
--- Python/Revision/Revision.py
+++ Python/Revision/Revision.py
@@ -1,2 +1,12 @@
-def inverse(y):
-    pass
+i = 1
+n = int(input("Enter numebr here: "))
+while i < n+1:
+    for _ in range(n-i):
+        print("", end = " ")
+
+    for _ in range(i): # [0, 1, 2]
+        print(i, end = " ")
+
+    print()
+    
+    i += 1
