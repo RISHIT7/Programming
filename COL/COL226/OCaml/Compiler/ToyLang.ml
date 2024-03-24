6f7b2b1995c5d6e010b7bb2f3a83805174be36bc
Rishit Jakharia <rishitjakharia@gmail.com>
2024-03-25 01:56:39 +0530

    Let expressions added

--- COL/COL226/OCaml/Compiler/ToyLang.ml
+++ COL/COL226/OCaml/Compiler/ToyLang.ml
@@ -7,6 +7,10 @@ type exp =
 | Eq of exp * exp | Gt of exp * exp (* Comparison Operators *)
 | IfTE of exp * exp * exp | Case of exp * (exp list) (* Conditionals *)
 | Pair of exp * exp | Fst of exp | Snd of exp (* Pair *)
+| Lexp of defs * exp
+
+and defs = Adef of string * exp | Pdef of defs * defs 
+          | Sdef of defs * defs  | Ldef of defs * defs (* definitions and local definitions *)
 ;;
 
 type values = N of int | B of bool | P of values * values;;
@@ -16,6 +20,7 @@ type opcode =   LDN of int | LDB of bool | LOOKUP of string
               | AND | OR | NOT | EQ | GT 
               | COND of opcode list * opcode list | CASE of (opcode list) list
               | PAIR | FST | SND
+              | LEXP of defs | NI
 ;;
 
 let myBool2bool b = match b with 
@@ -23,7 +28,15 @@ let myBool2bool b = match b with
 | F -> false
 ;;
 
-type defs = Adef of string * exp | Pdef of defs * defs | Sdef of defs * defs | Ldef of defs * defs (* definitions and local definitions *)
+exception Stuck of ((string * values) list) * values list * opcode list;; 
+exception Invalid_Var of string;;
+exception Invalid_Def of defs;;
+
+let rec get_elements_until_ni lst =
+   match lst with
+   | [] -> []
+   | NI :: _ -> []
+   | head :: tail -> head :: get_elements_until_ni tail
 ;;
 
 let rec compile e = match e with 
@@ -49,11 +62,9 @@ let rec compile e = match e with
 | Pair(e1, e2) -> (compile e1) @ (compile e2) @ [PAIR]
 | Fst(e1) -> compile e1 @ [FST]
 | Snd(e1) -> compile e1 @ [SND]
-;; 
-
-exception Stuck of ((string * values) list) * values list * opcode list;; 
-exception Invalid_Var of string;;
-exception Invalid_Def of defs;;
+(* Let def in exp ni *)
+| Lexp(d1, e1) -> [LEXP (d1)] @ (compile e1) @ [NI]
+;;
 
 let rec stkmc g s c = match s, c with 
 (* Values *)
@@ -80,12 +91,17 @@ let rec stkmc g s c = match s, c with
 | (a)::(b)::s', PAIR::c' -> stkmc g (P(b, a)::s') c'
 | (P (a, b))::s', FST::c' -> stkmc g (a::s') c'
 | (P (a, b))::s', SND::c' -> stkmc g (b::s') c'
+(* Let def in exp ni *)
+| s, LEXP(d1)::c' ->
+   let c1, rest_c = List.partition (fun x -> x <> NI) c' in
+   let a = stkmc (ret_gamma d1 g) [] (c1 @ [NI]) in 
+   stkmc g (a::s) rest_c
+| s, NI::c' -> stkmc g s c'
 (* Error *)
 | _, _ -> raise (Stuck (g, s, c)) 
-;; 
 
-let ret_gamma (test : defs) (prev_gamma : (string*values) list) : (string*values) list = 
-   let rec ret_gamma_helper test prev_gamma = match test with
+and ret_gamma (definition : defs) (prev_gamma : (string*values) list) : (string*values) list = 
+   let rec ret_gamma_helper definition prev_gamma = match definition with
    | Adef(a, b) -> [(a, stkmc prev_gamma [] (compile b))]
    | Pdef(d1, d2) -> (ret_gamma_helper d1 prev_gamma)@(ret_gamma_helper d2 prev_gamma)
    | Sdef (d1, d2) ->
@@ -94,8 +110,7 @@ let ret_gamma (test : defs) (prev_gamma : (string*values) list) : (string*values
    | Ldef (d1, d2) ->
       let val_d1 = ret_gamma_helper d1 prev_gamma in
       (ret_gamma_helper d2 (val_d1 @ prev_gamma))
-   | _ -> raise (Invalid_Def test)
-in (ret_gamma_helper test prev_gamma)@prev_gamma
+in (ret_gamma_helper definition prev_gamma)@prev_gamma
 ;;
 
 let test1a = Plus (Times (Num 3, Num 4), Times (Num 5, Num 6));; 
@@ -130,9 +145,10 @@ let test17b = Sdef(test14b, test15b);; (* 62, 282 *)
 let test18a = Ldef(test14a, test15a);; (* 42 *)
 let test18b = Ldef(test14b, test15b);; (* 282 *)
 
+let test19 = Lexp(test15a, Plus(V "z", V "x"));;
 
 let gamma = [("x", N 2); ("y", B true); ("x", N 3)];;
-let compiler = compile test10;;
+let compiler = compile test19;;
 let stack = stkmc gamma [] compiler;;
 
 let gamma_test = ret_gamma test18b [("x", N 7)];;
