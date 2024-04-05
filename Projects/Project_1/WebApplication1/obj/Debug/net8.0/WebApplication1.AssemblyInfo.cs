5dce76030dbb2da6770ade4e909f88e476a15bf1
Rishit Jakharia <rishitjakharia@gmail.com>
2024-04-05 14:42:54 +0530

    manage

--- Projects/Project_1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Projects/Project_1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695216dfce5c7ced4aa8ad7e4d877befc2ea9515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9569eb7729062c54854ee8c0189dc308571a84e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
