7ffcbbe54333c90f434efe26911e1bb24713c5ab
Rishit Jakharia <rishitjakharia@gmail.com>
2024-04-10 00:19:39 +0530

    game updated

--- Projects/Project_1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Projects/Project_1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3556945774356c5cfa2a33d44acd8095aea529a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1e4f2f1e9a30af4491962f517df1a410ee3f00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
