e5d492fe9c2fb30787288e061dae7461bc82e6e0
Rishit Jakharia <rishitjakharia@gmail.com>
2024-04-21 02:22:04 +0530

    Added modify and variable logic in interpreter

--- COL/COL226/OCaml/Prolog_Interpreter/Interpreter.ml
+++ COL/COL226/OCaml/Prolog_Interpreter/Interpreter.ml
@@ -2,7 +2,7 @@ open Printf
 
 type var = string
 type symbol = string
-type sig = (symbol * int) list
+type signature = (symbol * int) list
 type term =
     V of var
   | N of int
@@ -24,7 +24,7 @@ exception NotPossible
 
 let rec mem x y = match y with
     [] -> false
-  | z::rest = (x = z) || (mem x rest)
+  | z::rest -> (x = z) || (mem x rest)
 ;;
 
 let rec fold__left func e list = match list with
@@ -39,9 +39,8 @@ let rec map func list = match list with
 
 let rec combine l1 l2 = match l1, l2 with
     [], [] -> []
-  | x, [] -> x
-  | [], x -> x
   | x::rest1, y::rest2 -> (x, y)::combine rest1 rest2
+  | _, _ -> raise InvalidProgram
 ;;
 
 let rec union l1 l2 = match l1 with
@@ -56,3 +55,47 @@ let rec checkProgram (prog: program): bool = match prog with
     | Atom(">", _)   | Atom("<", _) -> raise InvalidProgram
     | _ -> checkProgram rest
 ;;
+
+let rec modifyTerm (i: int) (t: term): term = match t with
+    V(v) -> V((string_of_int i) ^ v)
+  | Node(s, l) -> Node(s, map (modifyTerm i) l)
+  | _ -> t
+;;
+
+let rec modifyAtom (i: int) (a: atom): atom = match a with
+    Atom(s, l) -> Atom(s, map (modifyTerm i) l)
+;;
+
+let rec modifyClause (i: int) (cl: clause): clause = match cl with
+    Fact(Head(a)) -> Fact(Head(modifyAtom i a))
+  | Rule(Head(a), Body(al)) -> Rule(Head(modifyAtom i a), Body(map (modifyAtom i) al))
+;;
+
+let rec modifyInitialProg (i: int) (prog: program): program = match prog with
+    [] -> []
+  | cl::rest -> (modifyClause i cl)::modifyInitialProg (i+1) prog
+;;
+
+let rec modifyProg2 (Atom(s, _): atom) (prog: program): program = match prog with
+    [] -> []
+  | cl::rest -> match cl with Fact(Head(Atom(s', _))) | Rule(Head(Atom(s', _)), _) ->
+                if s = s' then (modifyClause 0 cl)::modifyProg2 (Atom(s, [])) rest
+                else cl::modifyProg2 (Atom(s, [])) rest
+;;
+
+let rec vars_term (t: term): var list = 
+  match t with
+    V(v) -> [v]
+  | Node(s, l) -> fold__left union [] (map vars_term l)
+  | _ -> []
+;;
+
+let vars_atom (Atom(s, l): atom): var list = vars_term (Node(s, l))
+;;
+
+let rec vars_goal (Goal(g): goal): var list = fold__left union [] (map vars_atom g)
+;;
+
+let interpret_goal (prog: program) (g: goal) = solve_goal prog g [] (vars_goal g)
+;;
+
--- COL/COL226/OCaml/Prolog_Interpreter/main.ml
+++ COL/COL226/OCaml/Prolog_Interpreter/main.ml
@@ -2,37 +2,29 @@ open Interpreter;;
 open Lexer;;
 open Parser;;
 
-let () = 
-  if Array.length Sys.argv <> 2 then
-    Printf.printf "Usage: %s <input_file>\n" Sys.argv.(0)
-  else
-    let filename = Sys.argv.(1) in
-    let ast = parse_file filename in
-    Interpreter.print_program ast
-    ;
-
-let parse_file filename = 
-  let input_channel = open_in filename in
-  let lexbuf = Lexing.from_channel input_channel in
-  try
-    let result = Parser.program Lexer.token lexbuf in
-    let prog = modifyInitialProg result in
-    try 
-      while (true) do
-        print_string "?-";
-        let line = read_line() in
-        if line = "halt." then exit 0
-        else try
-          let g = Parser.goal Lexer.token (Lexing.from_string line) in
-          match (interpret_goal prog g) with
-          | (true, _) -> print_string "true.\n"
-          | (false, _) -> print_string "false.\n"
-        with e -> Printf.printf "%s\n" (Printexc.to_string e)
-      done;
-    close_in input_channel;
-    result
-  with
-  | Parsing.Parse_error ->
-    close_in input_channel;
-    failwith "Parsing Error"
+if Array.length Sys.argv <> 2 then begin
+  Printf.printf "Usage: %s <input_file>\n" Sys.argv.(0);
+  exit 0;
+end
 ;;
+
+let fstream = open_in Sys.argv.(1);;
+let init_prog = Parser.program Lexer.token (Lexing.from_channel fstream);;
+let _ = checkProgram init_prog;;
+let prog = modifyInitialProg 1 init_prog;;
+
+print_string "Prgram Loaded Successfully\n";;
+
+try
+  while (true) do
+    print_string "?-";
+    let line = read_line() in
+    if line = "halt." then exit 0
+    else try
+      let goal = Parser.goal Lexer.token (Lexing.from_string line) in
+      match (interpret_goal prog goal) with
+          (true, _) -> print_string "true.\n"
+        | (false, _) -> print_string "false.\n"
+    with e -> Printf.printf "%s\n" (Printexc.to_string e)
+  done
+with _ -> print_string "\n% halt\n"
