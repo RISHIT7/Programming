a41bc638e870d121c9e8e30f15df044b0753bcc8
Rishit Jakharia <rishitjakharia@gmail.com>
2024-04-21 01:25:42 +0530

    Refactor OCaml Prolog Interpreter code

--- COL/COL226/OCaml/Prolog_Interpreter/Interpreter.ml
+++ COL/COL226/OCaml/Prolog_Interpreter/Interpreter.ml
@@ -2,6 +2,7 @@ open Printf
 
 type var = string
 type symbol = string
+type sig = (symbol * int) list
 type term =
     V of var
   | N of int
@@ -14,75 +15,44 @@ type head = Head of atom
 type body = Body of atom list
 type clause = Fact of head | Rule of head * body
 type program = clause list
+type subs = (var * term) list
 
-let rec print_term (term : term) : unit = 
-  match term with
-  | V x -> Printf.printf "Variable: %s\n" x
-  | N n -> Printf.printf "Integer: %d\n" n
-  | B b -> Printf.printf "Boolean: %b\n" b
-  | Underscore -> Printf.printf "Variable: _\n"
-  | Node (s, terms) ->
-    Printf.printf "Term: %s\n\t" s;
-    print_term_list terms;
-    Printf.printf "\n"
-  
-  and print_term_list (terms : term list) : unit = 
-    match terms with
-    | [] -> ()
-    | [t] -> print_term t;
-    | t :: rest ->
-      print_term t;
-      Printf.printf "";
-      print_term_list rest;
+exception NOT_UNIFIABLE
+exception NotFound
+exception InvalidProgram
+exception NotPossible
+
+let rec mem x y = match y with
+    [] -> false
+  | z::rest = (x = z) || (mem x rest)
 ;;
 
-let rec print_atom (atom: atom) : unit = 
-  match atom with
-  | Atom (s, terms) -> 
-    Printf.printf "Atom: %s\n\t" s;
-    print_term_list terms;
-    Printf.printf "\n"
+let rec fold__left func e list = match list with
+    [] -> e
+  | x::rest -> fold__left func (func e x) rest
 ;;
 
-
-let print_head (head: head) : unit =
-  match head with
-  | Head atom -> print_atom atom
+let rec map func list = match list with
+    [] -> []
+  | x::rest -> (func x)::map func rest
 ;;
 
-let print_body (body: body) : unit =
-  match body with
-  | Body atoms ->
-      List.iter (fun atom -> print_atom atom; Printf.printf "") atoms
+let rec combine l1 l2 = match l1, l2 with
+    [], [] -> []
+  | x, [] -> x
+  | [], x -> x
+  | x::rest1, y::rest2 -> (x, y)::combine rest1 rest2
 ;;
 
-let print_clause (clause: clause) : unit =
-  match clause with
-  | Fact head ->
-      Printf.printf "Fact: \n";
-      print_head head
-  | Rule (head, body) ->
-      Printf.printf "Rule: \n";
-      print_head head;
-      Printf.printf "Cond\n";
-      print_body body
+let rec union l1 l2 = match l1 with
+    [] -> l2
+  | x::rest -> if (mem x l2) then union rest l2 else x::(union rest l2)
 ;;
 
-let print_program (program: program) : unit =
-  List.iter (fun clause -> print_clause clause; Printf.printf "\n") program
-;;
-
-let print_goal (goal: goal) : unit =
-  match goal with
-  | Goal atoms ->
-      List.iter (fun atom -> print_atom atom; Printf.printf "") atoms
-;;
-
-let rec print_ast ast =
-  match ast with
-  | [] -> Printf.printf "Empty AST\n"
-  | clause :: rest ->
-      print_clause clause;
-      Printf.printf "\n";
-      print_ast rest
+let rec checkProgram (prog: program): bool = match prog with
+    [] -> true
+  | (Fact(Head(a)))::rest | (Rule(Head(a), _))::rest -> match a with
+      Atom("_eq", _) | Atom("_not_eq", _) | Atom("_cut", _)
+    | Atom(">", _)   | Atom("<", _) -> raise InvalidProgram
+    | _ -> checkProgram rest
 ;;
--- COL/COL226/OCaml/Prolog_Interpreter/main.ml
+++ COL/COL226/OCaml/Prolog_Interpreter/main.ml
@@ -1,19 +1,6 @@
-open Interpreter
-open Lexer
-open Parser
-
-let parse_file filename = 
-  let input_channel = open_in filename in
-  let lexbuf = Lexing.from_channel input_channel in
-  try
-    let result = Parser.program Lexer.token lexbuf in
-    close_in input_channel;
-    result
-  with
-  | Parsing.Parse_error ->
-    close_in input_channel;
-    failwith "Parsing Error"
-;;
+open Interpreter;;
+open Lexer;;
+open Parser;;
 
 let () = 
   if Array.length Sys.argv <> 2 then
@@ -22,3 +9,30 @@ let () =
     let filename = Sys.argv.(1) in
     let ast = parse_file filename in
     Interpreter.print_program ast
+    ;
+
+let parse_file filename = 
+  let input_channel = open_in filename in
+  let lexbuf = Lexing.from_channel input_channel in
+  try
+    let result = Parser.program Lexer.token lexbuf in
+    let prog = modifyInitialProg result in
+    try 
+      while (true) do
+        print_string "?-";
+        let line = read_line() in
+        if line = "halt." then exit 0
+        else try
+          let g = Parser.goal Lexer.token (Lexing.from_string line) in
+          match (interpret_goal prog g) with
+          | (true, _) -> print_string "true.\n"
+          | (false, _) -> print_string "false.\n"
+        with e -> Printf.printf "%s\n" (Printexc.to_string e)
+      done;
+    close_in input_channel;
+    result
+  with
+  | Parsing.Parse_error ->
+    close_in input_channel;
+    failwith "Parsing Error"
+;;
