06df7bdc4de045adbca45a7077632366b597b286
Rishit Jakharia <rishitjakharia@gmail.com>
2024-04-23 00:08:39 +0530

    minor bug fixes and syntax corrections

--- COL/COL226/OCaml/Prolog_Interpreter/Interpreter.ml
+++ COL/COL226/OCaml/Prolog_Interpreter/Interpreter.ml
@@ -1,21 +1,25 @@
-type variable = string
+type var = string
 type symbol = string
 type signature = (symbol * int) list
-type term = V of variable | N of int | Node of symbol * (term list) | Underscore
-type atom = Atom of symbol * (term list)
+type term =
+    V of var
+  | N of int
+  | Node of symbol * (term list)
+  | Underscore
+type atom = Atom of symbol * term list
+type goal = Goal of atom list
 type head = Head of atom
 type body = Body of atom list
 type clause = Fact of head | Rule of head * body
 type program = clause list
-type goal = Goal of atom list
-type substitution = (variable * term) list
+type substitution = (var * term) list
 
-exception NOT_UNIFIABLE
+exception NotUnifiable
 exception NotFound
 exception InvalidProgram
 exception NotPossible
 
-let rec print_term_list (tl:term list) = match tl with
+let rec print_term_list tl = match tl with
     [] -> Printf.printf ""
   | [t] -> print_term t
   | t::tls -> (
@@ -24,7 +28,7 @@ let rec print_term_list (tl:term list) = match tl with
       print_term_list tls;
     )
 
-and print_list_body (t:term) = match t with
+and print_list_body t = match t with
     Node("_empty_list", []) -> Printf.printf ""
   | Node("_list", [t1; Node("_empty_list", [])]) -> print_term t1
   | Node("_list", [t1; t2]) -> (
@@ -34,7 +38,7 @@ and print_list_body (t:term) = match t with
     )
   | _ -> raise NotPossible
 
-and print_term (t:term) = match t with
+and print_term t = match t with
     V(v) -> Printf.printf " %s " v
   | Node("_empty_list", []) -> Printf.printf " [] "
   | Node(s, []) -> Printf.printf " %s " s
@@ -52,75 +56,54 @@ and print_term (t:term) = match t with
   | Underscore -> Printf.printf " _ "
 ;;
 
-let rec exists x y = match y with
-    [] -> false
-  | z::ys -> (x = z) || (exists x ys)
+let union l1 l2 = 
+  let combined = l1 @ l2 in
+  List.sort_uniq compare combined
 ;;
 
-let rec foldl f e l = match l with
-    [] -> e
-  | x::xs -> foldl f (f e x) xs
-;;
-
-let rec map f l = match l with
-    [] -> []
-  | x::xs -> (f x)::map f xs
-;;
-
-let rec combine l1 l2 = match l1 with
-    [] -> []
-  | x::xs -> (x, (List.hd l2))::combine xs (List.tl l2)
-;;
-
-let rec union l1 l2 = match l1 with
-    [] -> l2
-  | x::xs -> if (exists x l2) then union xs l2
-             else x::(union xs l2)
-;;
-
-let rec modifyTerm (i:int) (t:term): term = match t with
+let rec modifyTerm i t = match t with
     V(v) -> V((string_of_int i) ^ v)
-  | Node(s, l) -> Node(s, map (modifyTerm i) l)
+  | Node(s, l) -> Node(s, List.map (modifyTerm i) l)
   | _ -> t
 ;;
 
-let rec modifyAtom (i:int) (a:atom): atom = match a with
-  | Atom(s, l) -> Atom(s, map (modifyTerm i) l)
+let rec modifyAtom i a = match a with
+  | Atom(s, l) -> Atom(s, List.map (modifyTerm i) l)
 ;;
 
-let rec modifyClause (cl:clause) (i:int): clause = match cl with
+let rec modifyClause cl i = match cl with
     Fact(Head(a)) -> Fact(Head(modifyAtom i a))
-  | Rule(Head(a), Body(l)) -> Rule(Head(modifyAtom i a), Body(map (modifyAtom i) l))
+  | Rule(Head(a), Body(l)) -> Rule(Head(modifyAtom i a), Body(List.map (modifyAtom i) l))
 ;;
 
-let rec modifyInitialProg (prog:program) (i:int): program = match prog with
+let rec modifyInitialProg prog i = match prog with
     [] -> []
   | cl::ps -> (modifyClause cl i)::modifyInitialProg ps (i+1)
 ;;
 
-let rec modifyProg2 (prog:program) (Atom(s, _): atom): program = match prog with
-    [] -> []
-  | cl::ps -> match cl with Fact(Head(Atom(s', _))) | Rule(Head(Atom(s', _)), _) ->
+let rec modifyProg2 prog a = match prog, a with
+    [], _ -> []
+  | cl::ps, Atom(s, _) -> match cl with Fact(Head(Atom(s', _))) | Rule(Head(Atom(s', _)), _) ->
                 if s = s' then (modifyClause cl 0)::modifyProg2 ps (Atom(s, []))
                 else cl::modifyProg2 ps (Atom(s, []))
 ;;
 
-let rec vars_term (t:term): variable list =
+let rec vars_term t =
   match t with
       V(v) -> [v]
-    | Node(s, l) -> foldl union [] (map vars_term l)
+    | Node(s, l) -> List.fold_left union [] (List.map vars_term l)
     | _ -> []
 ;;
 
-let vars_atom (Atom(s, l): atom): variable list = vars_term (Node(s, l))
+let vars_atom a = match a with Atom(s, l) -> vars_term (Node(s, l))
 ;;
 
-let rec vars_goal (Goal(g): goal): variable list = foldl union [] (map vars_atom g)
+let rec vars_goal goal = match goal with Goal(g) -> List.fold_left union [] (List.map vars_atom g)
 ;;
 
-let rec subst (s:substitution) (t:term): term =
+let rec subst s t =
   match t with
-      Node(s', l) -> Node(s', map (subst s) l)
+      Node(s', l) -> Node(s', List.map (subst s) l)
     | N(_) -> t
     | Underscore -> t
     | V(x) -> match s with
@@ -128,45 +111,38 @@ let rec subst (s:substitution) (t:term): term =
                 | s'::xs -> if fst s' = x then snd s' else subst xs t
 ;;
 
-let rec subst_atom (s:substitution) (Atom(s', l): atom): atom = Atom(s', map (subst s) l)
+let rec subst_atom s a = match a with Atom(s', l) -> Atom(s', List.map (subst s) l)
 ;;
 
-let rec variableInTerm (v:variable) (t:term): bool =
+let rec varInTerm v t =
   match t with
       V(x) -> x = v
-    | Node(s, l) -> foldl (||) false (map (variableInTerm v) l)
+    | Node(s, l) -> List.fold_left (||) false (List.map (varInTerm v) l)
     | _ -> false
 ;;
 
-let compose (s1:substitution) (s2:substitution): substitution =
-  let f s x = (fst x, subst s (snd x)) in (map (f s2) s1) @ s2
+let compose s1 s2 =
+  let f s x = (fst x, subst s (snd x)) in (List.map (f s2) s1) @ s2
 ;;
 
-let rec mgu_term (t1:term) (t2:term): substitution =
-  match (t1, t2) with
-      (V(x), V(y)) -> if x = y then []
-                      else [(x, V(y))]
-    | (Underscore, _) -> []
-    | (_, Underscore) -> []
-    | (V(x), Node(_, _)) -> if variableInTerm x t2 then raise NOT_UNIFIABLE
-                            else [(x, t2)]
-    | (Node(_, _), V(y)) -> if variableInTerm y t1 then raise NOT_UNIFIABLE
-                            else [(y, t1)]
-    | (N(n1), N(n2)) -> if n1 = n2 then [] else raise NOT_UNIFIABLE
-    | (N(n1), V(x)) -> [(x, t1)]
-    | (V(x), N(n2)) -> [(x, t2)] 
-    | (Node(s1, l1), Node(s2, l2)) ->
-        if s1 <> s2 || (List.length l1 <> List.length l2) then raise NOT_UNIFIABLE
-        else
-          let f s tt = compose s (mgu_term (subst s (fst tt)) (subst s (snd tt))) in
-          foldl f [] (combine l1 l2)
-    | _ -> raise NOT_UNIFIABLE
+let rec mgu_term t1 t2= match (t1, t2) with
+  | V x, V y when x = y -> []
+  | (N(n1), N(n2)) when n1 = n2 -> []
+  | (Underscore, _) | (_, Underscore) -> []
+  | V x, Node(_, _) | V x, N _ when not (varInTerm x t2) -> [(x, t2)]
+  | Node(_, _), V y | N _, V y when not (varInTerm y t1) -> [(y, t1)]  
+  | (Node(s1, l1), Node(s2, l2)) ->
+      if s1 <> s2 || (List.length l1 <> List.length l2) then raise NotUnifiable
+      else
+        let f s tt = compose s (mgu_term (subst s (fst tt)) (subst s (snd tt))) in
+        List.fold_left f [] (List.combine l1 l2)
+  | _, _ -> raise NotUnifiable
 ;;
 
 let mgu_atom (Atom(s1, l1): atom) (Atom(s2, l2): atom): substitution = mgu_term (Node(s1, l1)) (Node(s2, l2))
 ;;
 
-let rec getSolution (unif:substitution) (vars:variable list) = match vars with
+let rec getSolution (unif:substitution) (vars:var list) = match vars with
     [] -> []
   | v::vs ->
       let rec occurs l = match l with
@@ -213,22 +189,22 @@ let rec simplify_term (t:term): term = match t with
   | Node("+", [t1; t2]) -> (
       match ((simplify_term t1), (simplify_term t2)) with
           (N(n1), N(n2)) -> N(n1 + n2)
-        | _ -> raise NOT_UNIFIABLE
+        | _ -> raise NotUnifiable
     )
   | Node("-", [t1; t2]) -> (
       match ((simplify_term t1), (simplify_term t2)) with
           (N(n1), N(n2)) -> N(n1 - n2)
-        | _ -> raise NOT_UNIFIABLE
+        | _ -> raise NotUnifiable
     )
   | Node("*", [t1; t2]) -> (
       match ((simplify_term t1), (simplify_term t2)) with
           (N(n1), N(n2)) -> N(n1 * n2)
-        | _ -> raise NOT_UNIFIABLE
+        | _ -> raise NotUnifiable
     )
   | Node("/", [t1; t2]) -> (
       match ((simplify_term t1), (simplify_term t2)) with
           (N(n1), N(n2)) -> N(n1 / n2)
-        | _ -> raise NOT_UNIFIABLE
+        | _ -> raise NotUnifiable
       )
   | _ -> t
 ;;
@@ -238,18 +214,18 @@ let eval (a:atom) (unif:substitution): substitution = match a with
   | Atom("_not_equal", [t1; t2]) -> compose unif (mgu_term (simplify_term (subst unif t1)) (simplify_term (subst unif t2)))
   | Atom(">", [t1; t2]) -> (
         match (simplify_term (subst unif t1), simplify_term (subst unif t2)) with
-            (N(n1), N(n2)) -> if n1 > n2 then unif else raise NOT_UNIFIABLE
-          | _ -> raise NOT_UNIFIABLE
+            (N(n1), N(n2)) -> if n1 > n2 then unif else raise NotUnifiable
+          | _ -> raise NotUnifiable
     )
   | Atom("<", [t1; t2]) -> (
       match (simplify_term (subst unif t1), simplify_term (subst unif t2)) with
-          (N(n1), N(n2)) -> if n1 < n2 then unif else raise NOT_UNIFIABLE
-        | _ -> raise NOT_UNIFIABLE
+          (N(n1), N(n2)) -> if n1 < n2 then unif else raise NotUnifiable
+        | _ -> raise NotUnifiable
     )
   | _ -> unif
 ;;
 
-let rec solve_goal (prog:program) (g:goal) (unif:substitution) (vars:variable list): (bool * substitution) =
+let rec solve_goal (prog:program) (g:goal) (unif:substitution) (vars:var list): (bool * substitution) =
   match g with
       Goal([]) -> (
         printSolution (getSolution unif vars);
@@ -267,11 +243,11 @@ let rec solve_goal (prog:program) (g:goal) (unif:substitution) (vars:variable li
     | Goal(a::gs) -> match a with
           Atom("_equal", _) | Atom(">", _) | Atom("<", _) -> (
             try solve_goal prog (Goal(gs)) (eval a unif) vars
-            with NOT_UNIFIABLE -> (false, [])
+            with NotUnifiable -> (false, [])
           )
         | Atom("_not_equal", _) -> (
             try (false, eval a unif)
-            with NOT_UNIFIABLE -> solve_goal prog (Goal(gs)) unif vars
+            with NotUnifiable -> solve_goal prog (Goal(gs)) unif vars
           )
         | Atom("_ofcourse", _) -> let _ = solve_goal prog (Goal(gs)) unif vars in (true, [])
         | Atom("integer", [t]) -> (
@@ -290,7 +266,7 @@ let rec solve_goal (prog:program) (g:goal) (unif:substitution) (vars:variable li
                     match (solve_goal new_prog (Goal(gs)) u vars) with
                         (true, u') -> (true, u')
                       | _ -> iter ps
-                  with NOT_UNIFIABLE -> iter ps
+                  with NotUnifiable -> iter ps
                 )
               | Rule(Head(a'), Body(al)) -> (
                   try
@@ -298,7 +274,7 @@ let rec solve_goal (prog:program) (g:goal) (unif:substitution) (vars:variable li
                     match (solve_goal new_prog (Goal(al @ gs)) u vars) with
                         (true, u') -> (true, u')
                       | _ -> iter ps
-                  with NOT_UNIFIABLE -> iter ps
+                  with NotUnifiable -> iter ps
                 )
         in iter new_prog
 ;;
--- COL/COL226/OCaml/Prolog_Interpreter/main.ml
+++ COL/COL226/OCaml/Prolog_Interpreter/main.ml
@@ -10,7 +10,7 @@ end
 
 let init_prog = Parser.program Lexer.token (Lexing.from_channel (open_in Sys.argv.(1)));;
 let counter = ref 1;;
-let prog = modifyInitialProg init_prog 1;;
+let new_prog = modifyInitialProg init_prog 1;;
 
 Printf.printf "%s Loaded Successfully!!\n" Sys.argv.(1);;
 
@@ -18,13 +18,13 @@ try
   while (true) do
     Printf.printf "%d ?- " !counter;
     let line = read_line() in
+    counter := !counter + 1;
     if line = "halt." then exit 0
     else try
       let goal = Parser.goal Lexer.token (Lexing.from_string line) in
-      match (interpret_goal goal prog) with
+      match (interpret_goal goal new_prog) with
           (true, _) -> Printf.printf "true.\n"
         | (false, _) -> Printf.printf "false.\n";
-        counter := !counter + 1
     with e -> Printf.printf "%s\n" (Printexc.to_string e)
   done
 with _ -> print_string "\n% halt\n"
