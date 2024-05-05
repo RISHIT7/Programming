9a44cf3337541c48ce41608c50ce2c3404e00962
RISHIT7 <cs1221621@iitd.ac.in>
2024-05-05 17:57:11 +0530

    Refactor Prolog interpreter code and fix syntax errors

--- COL/COL226/Prolog/Compiler/MiniLang.pl
+++ COL/COL226/Prolog/Compiler/MiniLang.pl
@@ -1,14 +1,8 @@
 /* Interpreter */ 
 calculate(numeral(N),numeral(N)) :- integer(N).
 calculate(mybool(B), mybool(B)) :- member(B, [true, false]), !.
-calculate(plus(E1,E2), numeral(N)) :- 
- calculate(E1, numeral(N1)), 
- calculate(E2, numeral(N2)), 
- N is N1+N2. 
-calculate(times(E1,E2), numeral(N)) :- 
- calculate(E1, numeral(N1)), 
- calculate(E2, numeral(N2)), 
- N is N1*N2.
+calculate(plus(E1,E2), numeral(N)) :- calculate(E1, numeral(N1)), calculate(E2, numeral(N2)), N is N1+N2. 
+calculate(times(E1,E2), numeral(N)) :- calculate(E1, numeral(N1)), calculate(E2, numeral(N2)), N is N1*N2.
 
 logical_or(false, false, false) :- !.
 logical_or(_, _, true) :- !.
@@ -38,21 +32,10 @@ append([], L, L).
 append([X|R], L, [X|Z]):- append(R, L, Z).
 
 compile(numeral(N),[ldop(N)]) :- integer(N). 
-compile(plus(E1,E2),C ) :- 
- compile(E1, C1), 
- compile(E2, C2), 
- append(C1, C2, C3), 
- append(C3, [plusop], C). 
-compile(times(E1,E2), C) :- 
- compile(E1, C1),
- compile(E2, C2), 
- append(C1, C2, C3), 
- append(C3, [timesop], C).
+compile(plus(E1,E2),C ) :- compile(E1, C1), compile(E2, C2), append(C1, C2, C3), append(C3, [plusop], C). 
+compile(times(E1,E2), C) :- compile(E1, C1),compile(E2, C2), append(C1, C2, C3), append(C3, [timesop], C).
 
 stackmc( [A|S1], [ ], A). 
-stackmc( S, [ldop(N)|C], A) :- 
- stackmc([N|S], C, A). 
-stackmc( [N2|[N1|S]], [plusop|C], A ) :- 
- N is N1+N2, stackmc([N|S], C, A). 
-stackmc( [N2|[N1|S]], [timesop|C], A ) :- 
- N is N1*N2, stackmc([N|S], C, A).
+stackmc( S, [ldop(N)|C], A) :- stackmc([N|S], C, A). 
+stackmc( [N2|[N1|S]], [plusop|C], A ) :- N is N1+N2, stackmc([N|S], C, A). 
+stackmc( [N2|[N1|S]], [timesop|C], A ) :- N is N1*N2, stackmc([N|S], C, A).
